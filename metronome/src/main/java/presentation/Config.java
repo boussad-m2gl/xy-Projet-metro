59cf0db053f113cade111d4615b7f57b7598060b
eagle <eagle@eagle-Latitude-D630>
2013-11-04 10:32:58 +0100

    ADD config class

---
+++ metronome/src/main/java/presentation/Config.java
@@ -0,0 +1,9 @@
+package presentation;
+
+public class Config {
+
+	public static final int MinTempParMesure=2;
+	public static final int MaxTempParMesure=7;
+	public static final int MinTempo =60;
+	public static final int MaxTempo=120;
+}
